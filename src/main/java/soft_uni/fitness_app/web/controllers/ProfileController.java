50dd235aad7f69a4847f887d17638b9159365fc0
Alexandar Georgiev <georgievalex807@gmail.com>
2026-09-18 16:34:16 +0300

    feat:Make the get mehtods of the dashboard main functionalities

--- src/main/java/soft_uni/fitness_app/web/controllers/ProfileController.java
+++ src/main/java/soft_uni/fitness_app/web/controllers/ProfileController.java
@@ -1,6 +1,7 @@
 package soft_uni.fitness_app.web.controllers;
 
 import jakarta.servlet.http.HttpSession;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,16 +10,15 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.UUID;
 
 @Controller
-@RequestMapping("/subscriptions")
-public class SubscriptionController {
+@RequestMapping("/profile")
+public class ProfileController {
 
 
-    @GetMapping
-    public ModelAndView showSubscriptions() {
-        ModelAndView mav = new ModelAndView("subscriptions");
-       return mav;
+    @GetMapping()
+    public ModelAndView getProfile(HttpSession session) {
+        UUID userId = (UUID) session.getAttribute("userId");
+        ModelAndView mav = new ModelAndView("redirect:/" + userId + "/profile");
+        return mav;
 
     }
-
-
 }
